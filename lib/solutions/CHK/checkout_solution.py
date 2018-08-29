1331dc692446e9cc85493d30c19ffd3c0265f3de
sbx_user1060 <sbx_user1060@ip-10-128-55-36.eu-west-2.compute.internal>
2018-08-29 17:28:47 +0000

    Wed Aug 29 17:28:47 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,41 +2,30 @@ from collections import Counter
 import re
 import json
 
+
 def check_input(skus):
     return re.match("^[A-Z]*$", skus)
 
 
-def get_offer_price(amount, offer_amount, offer_price, regular_price):
+def get_offer_price(regular_price, amount, offer_amount, offer_price):
     (offer, regular) = divmod(amount, offer_amount)
     return offer * offer_price + regular * regular_price
 
 
-def get_a_price(amount):
+def get_a_price(regular_price, amount):
     # 3A for 130, 5A for 200
     (offer1, regular) = divmod(amount, 5)
     (offer2, regular) = divmod(regular, 3)
-    return offer1 * 200 + offer2 * 130 + regular * 50
+    return offer1 * 200 + offer2 * 130 + regular * regular_price
 
 
-def get_b_price(b_amount, e_amount=0):
+def get_b_price(regular_price, b_amount, e_amount=0):
     if b_amount > 0:
         b_free = e_amount // 2
-        return get_offer_price(b_amount - b_free, 2, 45, 30)
+        return get_offer_price(b_amount - b_free, 2, 45, regular_price)
     else:
         return 0
 
-
-def get_c_price(amount):
-    return amount * 20
-
-
-def get_d_price(amount):
-    return amount * 15
-
-#
-# def get_e_price(amount):
-#     return amount * 40
-#
 
 def get_f_price(amount):
     free = amount // 3
@@ -55,13 +44,12 @@ def checkout(skus):
         with open('skus.json') as f:
             pricesjson = json.load(f)
 
-        a = get_a_price(amounts['A'])
-        b = get_b_price(amounts['B'],
-                        amounts['E'])  # silly mistake... makes me wonder if amount_e shouldnt be optional!
-        c = get_c_price(amounts['C'])
-        d = get_d_price(amounts['D'])
+        a = get_a_price(pricesjson['A']['price'], amounts['A'])
+        b = get_b_price(pricesjson['B']['price'], amounts['B'], amounts['E'])
+        c = amounts['C'] * pricesjson['C']['price']
+        d = amounts['D'] * pricesjson['D']['price']
         e = pricesjson['E']['price'] * amounts['E']
         f = get_f_price(amounts['F'])
         return a + b + c + d + e + f
     else:
-        return -1
+        return -1
